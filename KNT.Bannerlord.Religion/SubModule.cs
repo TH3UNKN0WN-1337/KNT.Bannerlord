c7f8e17466dbb6d913bc991d8b88a628a1f81402
TheUnknown <seebruch.mirko@outlook.de>
2022-08-16 14:39:13 +0200

    Loading specific game types dynamically when loading the mod

--- KNT.Bannerlord.Religion/SubModule.cs
+++ KNT.Bannerlord.Religion/SubModule.cs
@@ -1,24 +1,90 @@
-﻿using Bannerlord.ButterLib.MBSubModuleBaseExtended;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bannerlord.ButterLib.MBSubModuleBaseExtended;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.MissionViews;
 
 namespace KNT.Bannerlord.Religion;
 
 public class SubModule : MBSubModuleBaseEx
 {
+    private Type[]? _types;
+
     protected override void OnSubModuleLoad()
     {
         base.OnSubModuleLoad();
 
+        _types = typeof(SubModule).Assembly.GetTypes();
     }
 
     protected override void OnSubModuleUnloaded()
     {
         base.OnSubModuleUnloaded();
 
+        _types = null;
     }
 
-    protected override void OnBeforeInitialModuleScreenSetAsRoot()
+    protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
     {
-        base.OnBeforeInitialModuleScreenSetAsRoot();
+        base.InitializeGameStarter(game, starterObject);
 
+        if (starterObject is CampaignGameStarter campaignGameStarter)
+        {
+            AddCampaignBehaviors(campaignGameStarter);
+        }
+
+        AddGameModels(starterObject);
+    }
+
+    public override void OnMissionBehaviorInitialize(Mission mission)
+    {
+        base.OnMissionBehaviorInitialize(mission);
+
+        AddMissionBehaviors(mission);
+        AddMissionViews(mission);
+    }
+
+    private void AddCampaignBehaviors(CampaignGameStarter campaignGameStarter)
+    {
+        var campaignBehaviors = GetTypesOf<CampaignBehaviorBase>();
+        foreach (var campaignBehavior in campaignBehaviors)
+        {
+            campaignGameStarter.AddBehavior((CampaignBehaviorBase) Activator.CreateInstance(campaignBehavior));
+        }
+    }
+
+    private void AddGameModels(IGameStarter campaignGameStarter)
+    {
+        var gameModels = GetTypesOf<GameModel>();
+        foreach (var gameModel in gameModels)
+        {
+            campaignGameStarter.AddModel((GameModel) Activator.CreateInstance(gameModel));
+        }
+    }
+
+    private void AddMissionBehaviors(Mission mission)
+    {
+        var missionBehaviors = GetTypesOf<MissionBehavior>();
+        foreach (var missionBehavior in missionBehaviors)
+        {
+            mission.AddMissionBehavior((MissionBehavior) Activator.CreateInstance(missionBehavior));
+        }
+    }
+
+    private void AddMissionViews(Mission mission)
+    {
+        var missionViews = GetTypesOf<MissionView>();
+        foreach (var missionView in missionViews)
+        {
+            mission.AddMissionBehavior((MissionView) Activator.CreateInstance(missionView));
+        }
+    }
+
+    private IEnumerable<Type> GetTypesOf<T>()
+    {
+        return _types!.Where(type => type.IsAssignableFrom(typeof(T)));
     }
 }
