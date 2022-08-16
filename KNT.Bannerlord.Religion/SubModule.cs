a0ac58eadbb19e177bb49a43a9986521e527f594
TheUnknown <seebruch.mirko@outlook.de>
2022-08-16 11:25:21 +0200

    Add project files.

---
+++ KNT.Bannerlord.Religion/SubModule.cs
@@ -0,0 +1,24 @@
+﻿using Bannerlord.ButterLib.MBSubModuleBaseExtended;
+
+namespace KNT.Bannerlord.Religion;
+
+public class SubModule : MBSubModuleBaseEx
+{
+    protected override void OnSubModuleLoad()
+    {
+        base.OnSubModuleLoad();
+
+    }
+
+    protected override void OnSubModuleUnloaded()
+    {
+        base.OnSubModuleUnloaded();
+
+    }
+
+    protected override void OnBeforeInitialModuleScreenSetAsRoot()
+    {
+        base.OnBeforeInitialModuleScreenSetAsRoot();
+
+    }
+}
